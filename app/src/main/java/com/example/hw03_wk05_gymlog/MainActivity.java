266c25c17663eae294c3e19e1de499510c960636
Rose Arias-Aceves <rose.arias7@protonmail.com>
2025-12-02 08:18:44 -0800

    End of video 9 Finished logout methods

--- app/src/main/java/com/example/hw03_wk05_gymlog/MainActivity.java
+++ app/src/main/java/com/example/hw03_wk05_gymlog/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.hw03_wk05_gymlog;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog.Builder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hw03_wk05_gymlog.database.GymLogRepository;
@@ -44,11 +53,10 @@ public class MainActivity extends AppCompatActivity {
     loginUser();
     invalidateOptionsMenu();
 
-    if(loggedInUserId == -1){
+    if (loggedInUserId == -1) {
       Intent intent = LoginActivity.logIntentFactory(getApplicationContext());
       startActivity(intent);
     }
-
 
     repository = GymLogRepository.getRepository(getApplication());
     binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
@@ -65,24 +73,78 @@ public class MainActivity extends AppCompatActivity {
   }
 
 
-
   private void loginUser() {
-    //TODO: create login method
-    loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID,-1);
+    //TODO: make login method functional
+    user = new User("Rose", "password");
+    loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
 
   }
 
-  static Intent mainActivityIntentFactory(Context context,int userId){
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.logout_menu, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onPrepareOptionsMenu(Menu menu) {
+    MenuItem item = menu.findItem(R.id.logoutMenuItem);
+    item.setVisible(true);
+    item.setTitle(user.getUsername());
+
+    item.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+      @Override
+      public boolean onMenuItemClick(@NonNull MenuItem item) {
+        showLogoutDialog();
+        return false;
+      }
+    });
+
+    return true;
+  }
+
+  private void showLogoutDialog() {
+    AlertDialog.Builder alertBuilder = new Builder(MainActivity.this);
+    final AlertDialog alertDialog = alertBuilder.create();
+
+    alertBuilder.setMessage("Logout?");
+
+    alertBuilder.setPositiveButton("logout", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        logout();
+      }
+    });
+    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        alertDialog.dismiss();
+      }
+    });
+
+    alertBuilder.create().show();
+  }
+
+
+  private void logout() {
+    //TODO: finish logout method
+    startActivity(LoginActivity.logIntentFactory(getApplicationContext()));
+
+  }
+
+
+  static Intent mainActivityIntentFactory(Context context, int userId) {
     Intent intent = new Intent(context, MainActivity.class);
     intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
     return intent;
   }
 
-  private void insertGymlogRecord(){
-    if(mExercise.isEmpty()){
+  private void insertGymlogRecord() {
+    if (mExercise.isEmpty()) {
       return;
     }
-    GymLog log = new GymLog(mExercise,mWeight,mReps,loggedInUserId);
+    GymLog log = new GymLog(mExercise, mWeight, mReps, loggedInUserId);
     repository.insertGymLog(log);
 
   }
@@ -90,12 +152,12 @@ public class MainActivity extends AppCompatActivity {
 
   private void updateDisplay() {
     ArrayList<GymLog> allLogs = repository.getAllLogs();
-    if(allLogs.isEmpty()){
+    if (allLogs.isEmpty()) {
       binding.logDisplayTextView.setText(R.string.nothing_to_show_time_to_hit_the_gym);
     }
 
     StringBuilder sb = new StringBuilder();
-    for(GymLog log : allLogs){
+    for (GymLog log : allLogs) {
       sb.append(log);
     }
 
